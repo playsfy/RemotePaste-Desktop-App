58d2dec36fc9249fb82cb246cae3a0fe12787a46
katheesh <katheeskumar@outlook.com>
2020-12-27 22:41:09 +0530

    added new login api

--- main.js
+++ main.js
@@ -1,54 +1,56 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { autoUpdater } = require('electron-updater');
+const { app, Menu, BrowserWindow, ipcMain } = require("electron");
+const { autoUpdater } = require("electron-updater");
 
 let mainWindow;
 
-function createWindow () {
+Menu.setApplicationMenu(null);
+
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 650,
     height: 380,
     webPreferences: {
       nodeIntegration: true,
     },
-    icon:'build/icons/icon.png'
+    icon: "build/icons/icon.png",
   });
-  mainWindow.loadFile('views/index.html');
-  mainWindow.on('closed', function () {
+  mainWindow.loadFile("views/index.html");
+  mainWindow.on("closed", function () {
     mainWindow = null;
   });
-  mainWindow.once('ready-to-show', () => {
+  mainWindow.once("ready-to-show", () => {
     autoUpdater.checkForUpdatesAndNotify();
   });
 }
 
-app.on('ready', () => {
+app.on("ready", () => {
   createWindow();
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', function () {
+app.on("activate", function () {
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
 });
 
-autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update_available');
+autoUpdater.on("update-available", () => {
+  mainWindow.webContents.send("update_available");
 });
 
-autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
+autoUpdater.on("update-downloaded", () => {
+  mainWindow.webContents.send("update_downloaded");
 });
 
-ipcMain.on('restart_app', () => {
+ipcMain.on("restart_app", () => {
   autoUpdater.quitAndInstall();
 });
